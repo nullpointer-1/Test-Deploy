3e8d952a57679d877a89ee967b4b7c5e426e45b3
nullpointer-1 <pointersnull77@gmail.com>
2025-04-10 00:02:57 +0530

    Update AutoShopApplication.java

--- src/main/java/com/vehicle/car/AutoShopApplication.java
+++ src/main/java/com/vehicle/car/AutoShopApplication.java
@@ -7,10 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AutoShopApplication {
 
 	public static void main(String[] args) {
-		System.out.println("DB_URL: " + System.getenv("DB_URL"));
-System.out.println("DB_USERNAME: " + System.getenv("DB_USERNAME"));
-System.out.println("DB_PASSWORD: " + System.getenv("DB_PASSWORD"));
-
 		SpringApplication.run(AutoShopApplication.class, args);
 	}
 
